eeeaa44832da9a4ce63a3e45199d3d15985f306e
Aviator 2277 <91645065+Aviator2276@users.noreply.github.com>
2024-03-02 17:02:04 -0500

    Fixed swerveUtils import issue

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -13,15 +13,13 @@ import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.util.WPIUtilJNI;
-import edu.wpi.first.wpilibj.ADIS16470_IMU;
 import frc.robot.Constants.DriveConstants;
-import frc.utils.SwerveUtils;
+import utils.SwerveUtils;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.DriverStation;
 
 public class DriveSubsystem extends SubsystemBase {
   // Create MAXSwerveModules
--- src/main/java/utils/SwerveUtils.java
+++ src/main/java/utils/SwerveUtils.java
@@ -1,4 +1,4 @@
-package frc.utils;
+package utils;
 
 public class SwerveUtils {
 
